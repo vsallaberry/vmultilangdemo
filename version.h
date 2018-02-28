0b9e6c2bf1281664cd69ccf88dc9fa47d7804406
vincent <vsallaberry@gmail.com>
2018-02-28 16:45:03 +0100

    FIX [Makefile] fixes and improvements
    * fix Build.java syntax
    * keep original git revision when making a dist with 'make dist'
    * start 'make install' with files to install in the variable
    * INSTALL_FILES
    * remove dirty wotkaround for bison header deps on first build
    by defining a default OBJ dependency to all headers when .d files are
    not ready.

--- version.h
+++ version.h
@@ -1,7 +1,10 @@
 #ifndef APP_VERSION_H
 #define APP_VERSION_H
-#define APP_VERSION "0.2"
+#define APP_VERSION "0.3.0"
 #define APP_INCLUDE_SOURCE
 #define APP_BUILD_NUMBER 1
+#define DIST_GITREV "unknown"
+#define DIST_GITREVFULL "unknown"
+#define DIST_GITREMOTE "unknown"
 #include "build.h"
 #endif
